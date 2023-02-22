97b20af0c03f4cc55b16c5fe81279b4ca6668466
Basem-Serag <68796093+Basem-Serag@users.noreply.github.com>
2023-02-22 17:46:49 +0200

    add all logic and make it more easy

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,55 @@
-import { Component, VERSION } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
+export class AppComponent {
+  lenght: number = 0;
+  includeLetters: boolean = false;
+  includeNumbers: boolean = false;
+  includeSymblos: boolean = false;
+  generatedPassword: string = '';
+  constructor() {}
+
+  onLengthChange(value: string) {
+    const parcedValue = parseInt(value);
+    if (!isNaN(parcedValue)) {
+      this.lenght = parcedValue;
+    }
+  }
+  onLettersChange() {
+    this.includeLetters = !this.includeLetters;
+  }
+  onNumbersChange() {
+    this.includeNumbers = !this.includeNumbers;
+  }
+  onSymbolsChange() {
+    this.includeSymblos = !this.includeSymblos;
+  }
+
+  onGenerate() {
+    const numbers = '1234567890';
+    const letters = 'abcdefghijklmnopqrstwxyz';
+    const symbols = '!@#$%^&*()_';
+
+    let validValue = '';
+    if (this.includeLetters) {
+      validValue += letters;
+    }
+    if (this.includeNumbers) {
+      validValue += numbers;
+    }
+    if (this.includeSymblos) {
+      validValue += symbols;
+    }
+
+    let generatedValues = '';
+    for (let i = 0; i < this.lenght; i++) {
+      const index = Math.floor(Math.random() * validValue.length);
+      generatedValues += validValue[index];
+    }
+    this.generatedPassword = generatedValues;
+  }
 }
